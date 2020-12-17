0ddaa4763db6e4c73ac034e4f3712eb026f0ff4f
Waschington Luiz Menezes <wmenezes@elfsm.local>
2020-12-17 13:49:16 -0300

    refactor product router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
-import express from express;
+import express from 'express';
+import productsRoute from './products';
 
 const router = express.Router();
+
+router.use('/products', productsRoute);
+router.get('/', (req, res) => res.send('Hello World!'));
 
 export default router;
