059c542fe63b7309ca0d5987960c5964cf6a32af
wasmenezes <was288519@gmail.com>
2020-12-17 05:52:16 -0300

    install mocha, chai and supertest

--- src/server.js
+++ src/server.js
@@ -4,8 +4,11 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', function (req, res, next) {
+  console.log('route / called');
+  res.send('Hello World!');
+});
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('app is running');
 });
