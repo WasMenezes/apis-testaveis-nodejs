23549f5b34ba47b3d7968c986765967cc4d142da
wasmenezes <was288519@gmail.com>
2020-12-18 06:20:43 -0300

    refactor server and connect mongodb atlas

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost/test';
+const mongodbUrl = process.env.MONGODB_URL || 'mongodb+srv://user:livro123@cluster0.vxmqm.mongodb.net/dbLivroApis?retryWrites=true&w=majority';
 
 const connect = () => mongoose.connect(mongodbUrl, {
   useNewUrlParser: true,
